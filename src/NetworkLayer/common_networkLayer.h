7a9a97dcde26dfde81a66f660052cf5903883e98
PickingUpPieces <v.picking@gmx.de>
2021-06-27 17:42:26 +0200

    Add Message struct for passing meta information between objects

--- src/NetworkLayer/common_networkLayer.h
+++ src/NetworkLayer/common_networkLayer.h
@@ -2,13 +2,23 @@
 #define REPLICATIONNODE_COMMON_NETWORKLAYER_H
 
 #include <stdio.h>
+#include "rpc.h"
 
-const size_t max_message_size = 4096;
+const size_t maxMessageSize = 4096;
 
-enum messageType {
+enum MessageType {
     READ = 2,
     APPEND = 3
 };
 
+struct Message {
+    MessageType messageType;
+    uint64_t logOffset;
+    erpc::ReqHandle *reqHandle;
+    void *reqBuffer;
+    size_t reqBufferSize;
+    void *respBuffer;
+    size_t respBufferSize;
+};
 
 #endif // REPLICATIONNODE_COMMON_NETWORKLAYER_H
