41c912a9bcb8d0fffa8413f7658914777e294450
PickingUpPieces <v.picking@gmx.de>
2021-07-10 00:23:00 +0200

    Copy the right logOffset

--- tests/run_node.cpp
+++ tests/run_node.cpp
@@ -35,12 +35,11 @@ void receive_locally(Message *message) {
 
     if (message->messageType == READ) {
         string uniqueString = randomString + "-ID-" + std::to_string(message->logOffset);
-        DEBUG_MSG("generatedString: " << uniqueString);
+        DEBUG_MSG("generatedString vs returnedString: " << uniqueString << " vs " << ((LogEntryInFlight *) message->respBuffer.buf)->logEntry.data );
 
         if (strncmp(uniqueString.c_str(), ((LogEntryInFlight *) message->reqBuffer->buf)->logEntry.data, uniqueString.length()) == 0)
             messagesValidated_++;
     }
-
 
     DEBUG_MSG("run_node.receive_locally(messagesInFlight_: " << std::to_string(messagesInFlight_) << "messagesSent_: " << std::to_string(messagesSent_) << " ; messagesFinished_: " << std::to_string(messagesFinished_) << " ; messagesValidated_: " << std::to_string(messagesValidated_) << ")");
     localNode->NetworkManager_->rpc_.free_msg_buffer(*(message->reqBuffer));
@@ -65,9 +64,9 @@ void send_read_message(uint64_t logOffset) {
 
     DEBUG_MSG("run_node.send_read_message(Message: Type: " << std::to_string(message->messageType) << "; logOffset: " << std::to_string(message->logOffset) << " ; sentByThisNode: " << message->sentByThisNode << " ; reqBufferSize: " << std::to_string(message->reqBufferSize) << " ; respBufferSize: " << std::to_string(message->respBufferSize) <<")");
 
-    if ( node == HEAD )
+    if (node == HEAD)
         localNode->read(message);
-    else 
+    else
         localNode->NetworkManager_->send_message(HEAD, message);
 
     messagesInFlight_++;
@@ -75,7 +74,7 @@ void send_read_message(uint64_t logOffset) {
     DEBUG_MSG("run_node.send_read_message(messagesInFlight_: " << std::to_string(messagesInFlight_) << " ; messagesSent_: " << std::to_string(messagesSent_) << ")");
 }
 
-uint64_t send_append_message(uint64_t logOffset, void *data, size_t dataLength) {
+void send_append_message(void *data, size_t dataLength) {
     message = (Message *) malloc(sizeof(Message));
     reqRead = (erpc::MsgBuffer *) malloc(sizeof(erpc::MsgBuffer));
     *reqRead = localNode->NetworkManager_->rpc_.alloc_msg_buffer_or_die(MAX_MESSAGE_SIZE);
@@ -86,12 +85,10 @@ uint64_t send_append_message(uint64_t logOffset, void *data, size_t dataLength) 
 	message->respBuffer = localNode->NetworkManager_->rpc_.alloc_msg_buffer_or_die(MAX_MESSAGE_SIZE);
 	message->respBufferSize = MAX_MESSAGE_SIZE;
     message->sentByThisNode = true;
-    message->logOffset = logOffset;
     message->messageType = APPEND;
     memcpy(message->reqBuffer->buf, data, dataLength);
 
-    DEBUG_MSG("run_node.send_append_message(Message: Type: " << std::to_string(message->messageType) << "; logOffset: " << std::to_string(message->logOffset) << " ; sentByThisNode: " << message->sentByThisNode << " ; reqBufferSize: " << std::to_string(message->reqBufferSize) << " ; respBufferSize: " << std::to_string(message->respBufferSize) <<")");
-    uint64_t returnLogOffset = logOffset;
+    DEBUG_MSG("run_node.send_append_message(Message: Type: " << std::to_string(message->messageType) << "; logOffset: " << " ; sentByThisNode: " << message->sentByThisNode << " ; reqBufferSize: " << std::to_string(message->reqBufferSize) << " ; respBufferSize: " << std::to_string(message->respBufferSize) <<")");
 
 	if ( node == HEAD )
         localNode->append(message);
@@ -101,39 +98,34 @@ uint64_t send_append_message(uint64_t logOffset, void *data, size_t dataLength) 
     messagesInFlight_++;
     messagesSent_++;
     DEBUG_MSG("run_node.send_append_message(messagesInFlight_: " << std::to_string(messagesInFlight_) << " ; messagesSent_: " << std::to_string(messagesSent_) << ")");
-
-    return returnLogOffset;
 }
 
 void testing(Modus modus) {
     DEBUG_MSG("-------------------------------------");
-    //DEBUG_MSG("Start testing...");
     uint64_t counter{0};
-    uint64_t returnedLogOffset;
 
     /* Generate random string */
     string possibleCharacters = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     mt19937 generator{random_device{}()};
     uniform_int_distribution<> dist(0, possibleCharacters.size()-1);
     for(int i = 0; i < 16; i++){
-        int random_index = dist(generator); //get index between 0 and possible_characters.size()-1
+        size_t random_index = dist(generator); //get index between 0 and possible_characters.size()-1
         randomString += possibleCharacters[random_index];
     }
 
 
-
     while (true) {
         /* Create string and LogEntryInFlight */
-        string uniqueString = randomString + "-ID-" + std::to_string(counter);
-        LogEntryInFlight logEntryInFlight{counter, { 5, ""}};
+        string uniqueString = randomString + "-ID-";
+        LogEntryInFlight logEntryInFlight{counter, { 0, ""}};
         uniqueString.copy(logEntryInFlight.logEntry.data, uniqueString.length());
         logEntryInFlight.logEntry.dataLength = uniqueString.length();
 
         /* APPEND */
-        returnedLogOffset = send_append_message(counter, &logEntryInFlight, sizeof(logEntryInFlight));
+        send_append_message(&logEntryInFlight, sizeof(logEntryInFlight));
 
         /* READ and verify */
-        send_read_message(returnedLogOffset);
+        send_read_message(counter);
         ++counter;
 
         if(modus == SLOW)
@@ -149,12 +141,10 @@ void testing(Modus modus) {
         }
 
         while (messagesInFlight_ > 10000)
-            localNode->NetworkManager_->sync(100);
+            localNode->NetworkManager_->sync(1000);
 
         DEBUG_MSG("------------------------------------");
     }
-
-    localNode->NetworkManager_->sync(100);
 }
 
 int main(int argc, char** argv) {
@@ -169,7 +159,7 @@ int main(int argc, char** argv) {
         else if ( cmd_arg.compare("tail") == 0 )
             node = TAIL;
     }
-    DEBUG_MSG("This node is: " << node << "(HEAD= 0, MIDDLE= 1, TAIL= 2)");
+    DEBUG_MSG("This node is: " << node << "(HEAD=0, MIDDLE=1, TAIL=2)");
 
     switch(node) {
         case HEAD: localNode = new ReplicationManager(node, BILL_URI, std::string(), NARDOLE_URI, NARDOLE_URI, &receive_locally); break;
@@ -181,8 +171,7 @@ int main(int argc, char** argv) {
     if (ACTIVE_MODE)
         testing(SLOW);
     else {
-        while (true) {
+        while (true)
            localNode->NetworkManager_->sync(1000); 
-        }
     }
 }
