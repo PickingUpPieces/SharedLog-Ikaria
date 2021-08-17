1d60375e3fa8e59f6d2fbeca23f85d65eedde6f0
PickingUpPieces <v.picking@gmx.de>
2021-08-17 17:34:35 +0200

    Add filename generation

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -6,13 +6,14 @@ sleepTime=$(expr $runTime + 5)
 size=256
 threads=(1 2 4 8 16)
 readProb=(50 70 90)
-# TODO: Generate csv filename: timestamp-benchmark.csv 
+current_time=$(date "+%d-%H.%M.%S")
+fileName=$current_time"-benchmark.csv"
 
 echo $nodeID " " $runTime " " $size
 
 for r in ${readProb[@]}; do
     for t in ${threads[@]}; do
-        sudo ./../benchmark -i $nodeID -r $r -t $t -h $runTime
+        sudo ./../benchmark -i $nodeID -r $r -t $t -h $runTime -f $fileName
         sleep $sleepTime
     done
 done
