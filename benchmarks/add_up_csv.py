cc2192bfb1fd8fcbb449b9e4acbdb3505cfbb5d9
PickingUpPieces <v.picking@gmx.de>
2021-08-18 10:09:35 +0200

    fix python script

--- benchmarks/add_up_csv.py
+++ benchmarks/add_up_csv.py
@@ -1,5 +1,4 @@
 import string
-import csv
 from argparse import ArgumentParser
 import pandas as pd
 
@@ -7,12 +6,15 @@ parser = ArgumentParser()
 parser.add_argument('-n', '--names-list', nargs='+', default=[])
 files = parser.parse_args()
 
+dfs = [pd.read_csv(csv) for csv in files]
+
 node1 = pd.read_csv(files.names_list[0], sep=",")
 node2 = pd.read_csv(files.names_list[1], sep=",")
 node3 = pd.read_csv(files.names_list[2], sep=",")
 
-node1["reads"] = node1["reads"] + node2["reads"] + node3["reads"]
-node1["appends"] = node1["appends"] + node2["appends"] + node3["appends"]
-node1["ops"] = node1["ops"] + node2["ops"] + node3["ops"]
+for csv in dfs[1:]:
+    dfs[0]["reads"] = dfs[0]["reads"] + csv["reads"] 
+    dfs[0]["appends"] = dfs[0]["appends"] + csv["appends"] 
+    dfs[0]["ops"] = dfs[0]["ops"] + csv["ops"] 
 
-node1.to_csv("mergedCSV.csv")
+dfs[0].to_csv("merged-"+files.names_list[0], index=False)
--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -6,13 +6,13 @@ runTime=25
 sleepTime=5
 size=256
 #threads=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
-threads=(1 2 4 6 8 10 12 14 16)
-readProb=(50 90)
+threads=(1 2 4 6 8 10 12 14 16 18 20)
+readProb=(50 70 90)
 
 echo "NodeID: " $nodeID " RunTime: " $runTime " Value Size: " $size
 
 for r in ${readProb[@]}; do
-    fileName=$chainLength"-"$r"-"$nodeID"-benchmark.csv"
+    fileName=$nodeID"-"$r"-"$chainLength"-benchmark.csv"
     for t in ${threads[@]}; do
         echo "Current run: Read Prob: " $r " Threads: " $t 
 
