3ff05cbc9c77fd44b2b895b53f1251d596677521
PickingUpPieces <v.picking@gmx.de>
2021-08-21 13:58:39 +0000

    Fix run_benchmark script

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-chainLength=3
+chainLength=$2
 nodeID=$1
 runTime=20
 sleepTime=5
-size=256
+size=(256)
+#size=(64 256 1024 2048 4096)
+threads=(16)
 #threads=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
-threads=(1 2 4 6 8 10 12 14 16)
-readProb=(50 70 90)
+readProb=(50 80 90)
 
 echo "NodeID: " $nodeID " RunTime: " $runTime " Value Size: " $size
 
 for r in ${readProb[@]}; do
     fileName=$nodeID"-"$r"-"$chainLength"-benchmark.csv"
     for t in ${threads[@]}; do
-        echo "Current run: Read Prob: " $r " Threads: " $t 
+        for s in ${size[@]}; do
+            echo "Current run: Read Prob: " $r " Threads: " $t " Size: " $s
 
-        sudo ./../benchmark -i $nodeID -r $r -t $t -h $runTime -f $fileName
-        pid=$!
-        wait $pid
-        sleep $sleepTime
+            sudo ./../benchmark -i $nodeID -r $r -t $t -h $runTime -f $fileName -s $s
+            pid=$!
+            wait $pid
+            sleep $sleepTime
+        done
     done
 done
 
