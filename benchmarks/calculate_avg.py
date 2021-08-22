5c3ca624857cc4e31474c2a17b3e31a3a0d5bdb6
PickingUpPieces <v.picking@gmx.de>
2021-08-22 20:06:26 +0200

    add average program

--- benchmarks/calculate_avg.py
+++ benchmarks/calculate_avg.py
@@ -13,4 +13,8 @@ for csv in dfs[1:]:
     dfs[0]["appends"] = dfs[0]["appends"] + csv["appends"] 
     dfs[0]["ops"] = dfs[0]["ops"] + csv["ops"] 
 
-dfs[0].to_csv("merged-"+files.names_list[0], index=False)
+dfs[0]["reads"] = dfs[0]["reads"] / len(dfs)
+dfs[0]["appends"] = dfs[0]["appends"] / len(dfs)
+dfs[0]["ops"] = dfs[0]["ops"] / len(dfs)
+
+dfs[0].to_csv("average-"+files.names_list[0], index=False)
