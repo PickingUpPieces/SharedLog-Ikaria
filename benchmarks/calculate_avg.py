a14407290538924acf42b8a511be62fcd6d5b456
PickingUpPieces <v.picking@gmx.de>
2021-08-26 13:18:31 +0000

    Add latency benchmarks to csv

--- benchmarks/calculate_avg.py
+++ benchmarks/calculate_avg.py
@@ -12,9 +12,13 @@ for csv in dfs[1:]:
     dfs[0]["reads"] = dfs[0]["reads"] + csv["reads"] 
     dfs[0]["appends"] = dfs[0]["appends"] + csv["appends"] 
     dfs[0]["ops"] = dfs[0]["ops"] + csv["ops"] 
+    dfs[0]["lat_50"] = dfs[0]["lat_50"] + csv["lat_50"] 
+    dfs[0]["lat_99"] = dfs[0]["lat_99"] + csv["lat_99"] 
 
-dfs[0]["reads"] = dfs[0]["reads"] / len(dfs)
-dfs[0]["appends"] = dfs[0]["appends"] / len(dfs)
-dfs[0]["ops"] = dfs[0]["ops"] / len(dfs)
+dfs[0]["reads"] = round(dfs[0]["reads"] / len(dfs), 2)
+dfs[0]["appends"] = round(dfs[0]["appends"] / len(dfs), 2)
+dfs[0]["ops"] = round(dfs[0]["ops"] / len(dfs), 2)
+dfs[0]["lat_50"] = round(dfs[0]["lat_50"] / len(dfs), 2)
+dfs[0]["lat_99"] = round(dfs[0]["lat_99"] / len(dfs), 2)
 
 dfs[0].to_csv("average-"+files.names_list[0], index=False)
--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -2,17 +2,19 @@
 
 nodeID=$1
 chainLength=$2
+#numberOfRuns=(1 2 3)
 numberOfRuns=(1 2 3)
 #inFlightCap=(1000) 
-inFlightCap=(100 1000 5000 10000) 
-runTime=20
-sleepTime=2
+inFlightCap=(30 50 70) 
+runTime=30
+sleepTime=5
 #size=(256)
 #size=(64 256 1024 2048)
 size=(256)
 threads=(8)
 #threads=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
-readProb=(50 80 90)
+#readProb=(50 80 90)
+readProb=(50 90)
 
 echo "NodeID: " $nodeID " RunTime: " $runTime " Value Size: " $size
 
