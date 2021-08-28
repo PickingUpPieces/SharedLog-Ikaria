d593208197eb186b6d2030d8dcc25e831d661df2
PickingUpPieces <v.picking@gmx.de>
2021-08-28 21:47:01 +0200

    Round 0 in benchmark python scripts

--- benchmarks/add_up_csv.py
+++ benchmarks/add_up_csv.py
@@ -11,6 +11,6 @@ dfs = [pd.read_csv(csv) for csv in files.names_list]
 for csv in dfs[1:]:
     dfs[0]["reads"] = dfs[0]["reads"] + csv["reads"] 
     dfs[0]["appends"] = dfs[0]["appends"] + csv["appends"] 
-    dfs[0]["ops"] = dfs[0]["ops"] + csv["ops"] 
+    dfs[0]["ops"] = round(dfs[0]["ops"] + csv["ops"], 0)
 
 dfs[0].to_csv("merged-"+files.names_list[0], index=False)
--- benchmarks/calculate_avg.py
+++ benchmarks/calculate_avg.py
@@ -15,10 +15,10 @@ for csv in dfs[1:]:
     dfs[0]["lat_50"] = dfs[0]["lat_50"] + csv["lat_50"] 
     dfs[0]["lat_99"] = dfs[0]["lat_99"] + csv["lat_99"] 
 
-dfs[0]["reads"] = round(dfs[0]["reads"] / len(dfs), 2)
-dfs[0]["appends"] = round(dfs[0]["appends"] / len(dfs), 2)
-dfs[0]["ops"] = round(dfs[0]["ops"] / len(dfs), 2)
-dfs[0]["lat_50"] = round(dfs[0]["lat_50"] / len(dfs), 2)
-dfs[0]["lat_99"] = round(dfs[0]["lat_99"] / len(dfs), 2)
+dfs[0]["reads"] = round(dfs[0]["reads"] / len(dfs), 0)
+dfs[0]["appends"] = round(dfs[0]["appends"] / len(dfs), 0)
+dfs[0]["ops"] = round(dfs[0]["ops"] / len(dfs), 0)
+dfs[0]["lat_50"] = round(dfs[0]["lat_50"] / len(dfs), 0)
+dfs[0]["lat_99"] = round(dfs[0]["lat_99"] / len(dfs), 0)
 
 dfs[0].to_csv("average-"+files.names_list[0], index=False)
